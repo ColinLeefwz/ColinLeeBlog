810d522e09bfb19ed7719d8408d9b631accb437b
colin <lilonglongfwz@gmail.com>
2017-12-09 22:16:42 +0800

    [user] add encrypt to password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,31 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :password
+  validates :email, :presence => true, :uniqueness => true, :format => {:with  => Devise.email_regexp, :allow_blank => true, :if => :email_changed?}
+  validates :password, :confirmation => true, :length => { :minimum => 6, :maximum => 15 } #password_confirmation attr
+
+  before_save :encrypt_password
+  after_save :clear_password
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.encrypted_password == BCrypt::Engine.hash_secret(password, user.salt)
+      user
+    else
+      nil
+    end
+  end
+
+  def encrypt_password
+    if password.present?
+      self.salt = BCrypt::Engine.generate_salt
+      self.encrypted_password = BCrypt::Engine.hash_secret(password, salt)
+    end
+  end
+
+  def clear_password
+      self.password = nil
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "home#index"
 
